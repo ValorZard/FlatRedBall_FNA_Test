45442ea60c30555a0e06f9c60b8161b381015932
Srayan Jana <valorzard.kiko@outlook.com>
2024-11-13 11:25:41 -0800

    add flatredball, need to replace with new fna

---
+++ FlatRedBall_FNA_Test/GumRuntimes/Elements/DividerVerticalRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlatRedBall_FNA_Test.GumRuntimes.Elements
+{
+    public partial class DividerVerticalRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
---
+++ FlatRedBall_FNA_Test/Screens/Level1.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using FlatRedBall;
+using FlatRedBall.Input;
+using FlatRedBall.Instructions;
+using FlatRedBall.AI.Pathfinding;
+using FlatRedBall.Graphics.Animation;
+using FlatRedBall.Gui;
+using FlatRedBall.Math;
+using FlatRedBall.Math.Geometry;
+using FlatRedBall.Localization;
+using Microsoft.Xna.Framework;
+
+using FlatRedBall_FNA_Test.Entities;
+
+
+namespace FlatRedBall_FNA_Test.Screens
+{
+    public partial class Level1
+    {
+        private void CustomInitialize()
+        {
+            
+        }
+
+        private void CustomActivity(bool firstTimeCalled)
+        {
+            
+        }
+
+        private void CustomDestroy()
+        {
+            
+        }
+
+        private static void CustomLoadStaticContent(string contentManagerName)
+        {
+            
+        }
+    }
+}
